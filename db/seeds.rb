e2fb6c79f708e8795f4b9fec696b7c95940de7d1
betalantz <betalantz@gmail.com>
2020-02-15 18:03:58 -0800

    creates seeds for db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Note.create([
+    {body: 'Ipsam adipisci libero laudantium voluptas veniam rerum aliquid quasi.'},
+    {body: 'Aut quia sit aperiam consectetur deleniti esse quia nulla qui. Et velit dolor. Et natus tenetur veritatis quisquam tenetur tenetur repellendus omnis iste. Molestiae velit mollitia similique laudantium consequatur dolorem laboriosam et nihil. Adipisci consectetur sapiente. Sapiente vel excepturi at et harum nam.'},
+    {body: 'Quia magnam sequi nostrum non voluptatibus.'},
+    {body: 'Sint dolor magni sit molestias vitae voluptatum quos. Impedit a iure rerum debitis nostrum est minus rerum atque. Quam officiis et iusto ut. Sed fuga enim accusamus ullam quo dolorem a doloremque debitis. Officia qui fuga et. Quaerat natus ipsum et quo.'},
+    {body: 'Optio consequatur vero perspiciatis ratione rerum rerum.'},
+    {body: 'Architecto illo pariatur et.'},
+    {body: 'Aliquam est officia sed voluptatem placeat. Facilis blanditiis omnis velit est et voluptas sequi. Occaecati modi et quasi consequatur. Est sunt iste eum voluptas.'},
+    {body: 'Cupiditate blanditiis similique aliquid ipsum.'},
+    {body: 'Facere explicabo possimus velit voluptatibus tempora accusamus quisquam atque. Quo et omnis suscipit ut nesciunt dolore sunt cumque et. Repellat placeat doloremque ea. Recusandae illum quisquam qui. Dolor voluptas et nihil qui dignissimos.'},
+])
