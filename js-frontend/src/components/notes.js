26cd9f8def9af5f54f1bdb4f3a490af9c6bef16c
betalantz <betalantz@gmail.com>
2020-02-17 12:31:27 -0800

    adds render() to Notes class

---
+++ js-frontend/src/components/notes.js
@@ -0,0 +1,32 @@
+class Notes {
+    //where the meat of the program will live
+    constructor() {
+        this.notes = []
+        this.adapter = new NotesAdapter()
+        this.bindEventListeners()
+        this.fetchAndLoadNotes()
+    }
+
+    fetchAndLoadNotes() {
+        this.adapter.getNotes()
+        .then(notes => {
+            notes.forEach(note => {
+                this.notes.push(note)
+            });
+        })
+        .then(() => {
+            this.render()
+        })
+    }
+
+    bindEventListeners(){
+
+    }
+
+    render() {
+        const notesContainer = document.getElementById('notes-container')
+        notesContainer.innerHTML = 'my notes here'
+        console.log('my notes are...', this.notes);
+
+    }
+}
