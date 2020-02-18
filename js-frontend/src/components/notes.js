aa3b2d749e5b49141d026ee5ffb369590034dbd4
betalantz <betalantz@gmail.com>
2020-02-18 10:20:27 -0800

    makes notes editable on dblclick

--- js-frontend/src/components/notes.js
+++ js-frontend/src/components/notes.js
@@ -9,9 +9,12 @@ class Notes {
     
     initBindingsAndEventListeners(){
         this.notesContainer = document.getElementById('notes-container')
+        this.body = document.querySelector('body')
         this.newNoteBody = document.getElementById('new-note-body')
         this.noteForm = document.getElementById('new-note-form')
         this.noteForm.addEventListener('submit', this.createNote.bind(this))
+        this.notesContainer.addEventListener('dblclick', this.handleNoteClick.bind(this))
+        this.body.addEventListener('blur', this.updateNote.bind(this), true)
     }
 
     createNote(e) {
@@ -24,6 +27,16 @@ class Notes {
         })
     }
 
+    handleNoteClick(e) {
+        const li = e.target
+        li.contentEditable = "true"
+        li.focus()
+        li.classList.add('editable')
+    }
+
+    updateNote(e){
+        console.log('in updateNote()');
+    }
 
     fetchAndLoadNotes() {
         this.adapter
