51b1930bc658330457a685e28a3f380422ccc2d7
betalantz <betalantz@gmail.com>
2020-02-18 21:28:13 -0800

    creates user model, controller

--- notes-api/config/routes.rb
+++ notes-api/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  
   namespace :api do
     namespace :v1 do
+      get 'users/login', to: 'users#login'
       resources :notes
     end
   end
--- notes-api/db/schema.rb
+++ notes-api/db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_16_015223) do
+ActiveRecord::Schema.define(version: 2020_02_19_052302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "notes", force: :cascade do |t|
     t.string "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
